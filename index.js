c653eee3b2e515b4b2891617a571b72ea66834b8
Ken Barlow <kenbrlow@gmail.com>
2021-03-22 00:12:02 -0600

    commit tree 945a6f46554621397175a3923bef6343ce556a0f

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Add your functions here
+function map(array, functionToUse) {
+    return array.map(functionToUse);
+}
+
+function reduce(array, functionToUse, startPoint) {
+    let value;
+    if (startPoint != null) {
+      value = array.reduce(functionToUse, startPoint);
+    } else {
+      value = array.reduce(functionToUse);
+    }
+    return value;
+}
